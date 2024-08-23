132c2fdc22e4c7ec4c7052c919e4a228b7e0d342
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-22 20:01:44 -0500

    mostrar y ocultar el menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./Header.css";
 
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+  let showMenuStyle = showMenu ? 'show-menu' : '';
+
+  const handleShowMenu = () => {
+    setShowMenu(showMenu => !showMenu);
+  }
+
   return (
     <header className="header blur-header" id="header">
       <nav className="nav container">
@@ -9,43 +18,43 @@ const Header = () => {
           <span>Temple</span>
         </a>
 
-        <div className="nav__menu show-menu" id="nav-menu">
+        <div className={`nav__menu ${showMenuStyle}`} id="nav-menu">
           <ul className="nav__list">
             <li>
-              <a href="#" className="nav__link">
+              <a href="#" className="nav__link" onClick={handleShowMenu}>
                 Home
               </a>
             </li>
             <li>
-              <a href="#" className="nav__link">
+              <a href="#" className="nav__link" onClick={handleShowMenu}>
                 Location
               </a>
             </li>
             <li>
-              <a href="#" className="nav__link">
+              <a href="#" className="nav__link" onClick={handleShowMenu}>
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="nav__link">
+              <a href="#" className="nav__link" onClick={handleShowMenu}>
                 Explore
               </a>
             </li>
             <li>
-              <a href="#" className="nav__link">
+              <a href="#" className="nav__link" onClick={handleShowMenu}>
                 Contact
               </a>
             </li>
           </ul>
 
           {/* Close button */}
-          <div className="nav__close" id="nav-close">
+          <div className="nav__close" id="nav-close" onClick={handleShowMenu}>
             <i className="ri-close-large-line"></i>
           </div>
         </div>
 
         {/* Toggle button */}
-        <div className="nav__toggle" id="nav-toggle">
+        <div className="nav__toggle" id="nav-toggle" onClick={handleShowMenu}>
           <i className="ri-apps-2-line"></i>
         </div>
       </nav>
@@ -55,6 +64,4 @@ const Header = () => {
 
 export default Header;
 
-// TODO: Crear funcionalidad para mostrar y quitar el menú en tamaño pequeño (video 7:40)
-// TODO: Crear funcionalidad para que cuando se hace click en cualquiern item del menu, el menú se quite (video 8:05)
 // TODO: Crear funcionalidad para que cuando se haga scroll en la pantalla, el header se convierta en blur (video 21:12)
